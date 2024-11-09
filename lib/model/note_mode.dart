d2d2fb30445ecab84a15347cd7b8e684537cf829
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-09 23:59:57 +0600

    add note model

---
+++ lib/model/note_mode.dart
@@ -0,0 +1,41 @@
+// To parse this JSON data, do
+//
+//     final noteModel = noteModelFromJson(jsonString);
+
+import 'dart:convert';
+
+List<NoteModel> noteModelFromJson(String str) => List<NoteModel>.from(json.decode(str).map((x) => NoteModel.fromJson(x)));
+
+String noteModelToJson(List<NoteModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class NoteModel {
+  String title;
+  String description;
+  String date;
+  String time;
+  String id;
+
+  NoteModel({
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.time,
+    required this.id,
+  });
+
+  factory NoteModel.fromJson(Map<String, dynamic> json) => NoteModel(
+    title: json["title"],
+    description: json["description"],
+    date: json["date"],
+    time: json["time"],
+    id: json["id"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "title": title,
+    "description": description,
+    "date": date,
+    "time": time,
+    "id": id,
+  };
+}
