b6b808faf6c563f84b3d360d998f26360c605e1e
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-10 15:33:00 +0600

    add title and description function done

--- lib/controller/note_controller.dart
+++ lib/controller/note_controller.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 
 import 'package:crud_operation/model/note_mode.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:get/get.dart';
 
 class NoteController extends GetxController {
   String url = "https://673012ed66e42ceaf15f4989.mockapi.io/note";
   RxList<NoteModel> noteList = RxList<NoteModel>();
+  TextEditingController title = TextEditingController();
+  TextEditingController description = TextEditingController();
 
   @override
   void onInit() async {
@@ -20,6 +23,21 @@ class NoteController extends GetxController {
     for (var note in notes) {
       noteList.add(NoteModel.fromJson(note));
     }
-    print("Note Get");
+    print("Get Note");
+  }
+
+  Future<void> addNote() async {
+    var newNote = NoteModel(
+      title: title.text,
+      description: description.text,
+      date: DateTime.now().toString(),
+      time: "",
+    );
+    var response = await http.post(Uri.parse(url),
+        body: jsonEncode(newNote.toJson()),
+        headers: {"Content-Type": "Application/json"});
+    if (response.statusCode == 201) {
+      print("Add Note");
+    }
   }
 }
